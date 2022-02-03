6d45515d9b1199115cb69541901aace72c6e1a0e
Anton Gribanow <anton.gribanow@icloud.com>
2022-02-03 12:03:42 +0100

    Action Sheet Customization

--- MyPlaces/NewPlaceViewController.swift
+++ MyPlaces/NewPlaceViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class NewPlaceViewController: UITableViewController {
 
+    @IBOutlet var imageOfPlace: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -20,16 +22,23 @@ class NewPlaceViewController: UITableViewController {
            
            if indexPath.row == 0 {
                
+               let cameraIcon = #imageLiteral(resourceName: "camera")
+               let photoIcon = #imageLiteral(resourceName: "photo")
+               
                let actionSheet = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
                
                let camera = UIAlertAction(title: "Camera", style: .default) { _ in
-                   
+                
                    self.chooseImagePicker(source: .camera)
                }
+               camera.setValue(cameraIcon, forKey: "image") //dodajumy ikonki do przycisków
+               camera.setValue(CATextLayerAlignmentMode.left, forKey: "titleTextAlignment")
                
                let photo = UIAlertAction(title: "Photo", style: .default) { _ in
                    self.chooseImagePicker(source: .photoLibrary)
                }
+               photo.setValue(photoIcon, forKey: "image") //dodajumy ikonki do przycisków
+               photo.setValue(CATextLayerAlignmentMode.left, forKey: "titleTextAlignment")
                
                let cancel = UIAlertAction(title: "Cancel", style: .cancel)
                
@@ -59,15 +68,23 @@ extension NewPlaceViewController: UITextFieldDelegate {  //Hiding keyboard after
 
 // MARK: Work with image
 
-extension NewPlaceViewController {
+extension NewPlaceViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate{
     
     func chooseImagePicker(source: UIImagePickerController.SourceType) {
         
         if UIImagePickerController.isSourceTypeAvailable(source) {
             let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
             imagePicker.allowsEditing = true
             imagePicker.sourceType = source
             present(imagePicker, animated: true)
         }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) { // metoda wstawia obrazek na mejsce
+        imageOfPlace.image = info[.editedImage] as? UIImage
+        imageOfPlace.contentMode = .scaleAspectFill     //masztabujemy image
+        imageOfPlace.clipsToBounds = true               //tniemy image
+        dismiss(animated: true)                         //zamykamy imagePickerController
     }
 }
